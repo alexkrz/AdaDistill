72531492843cd29cccca918eca748f4940353ef7
Alexander Kurz <alex.kurz95@gmail.com>
2026-02-09 09:36:19 +0100

    Load model and print model summary

--- load_model.py
+++ load_model.py
@@ -1,9 +1,29 @@
+from dataclasses import dataclass
+from pathlib import Path
+
 import torch
+from torchinfo import summary
+
+from backbones.mobilefacenet import MobileFaceNet
+
+script_dir = Path(__file__).parent
 
 
-def main():
-    pass
+@dataclass
+class Config:
+    model_fp = str(script_dir / "output" / "AdaDistill" / "MFN_AdaArcDistill_backbone.pth")
+    input_size = (112, 112)
+    embedding_size = 512
+
+
+def main(args: Config):
+    model = MobileFaceNet(input_size=args.input_size, embedding_size=args.embedding_size)
+    weights = torch.load(args.model_fp, weights_only=True)
+    model.load_state_dict(weights)
+
+    summary(model, input_size=(1, 3, 112, 112))
 
 
 if __name__ == "__main__":
-    main()
+    args = Config
+    main(args)
